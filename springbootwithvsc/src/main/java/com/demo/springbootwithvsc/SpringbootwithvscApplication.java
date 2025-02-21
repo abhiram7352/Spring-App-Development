f67c0c023bb4a05eb5550a9ddb4046f87fb7e1fe
abhiram7352 <abhiramkumar8780@gmail.com>
2025-02-21 22:58:41 +0530

    [Abhiram] Added complete springboot project with vscode 🎉

---
+++ springbootwithvsc/src/main/java/com/demo/springbootwithvsc/SpringbootwithvscApplication.java
@@ -0,0 +1,13 @@
+package com.demo.springbootwithvsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootwithvscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootwithvscApplication.class, args);
+	}
+
+}
